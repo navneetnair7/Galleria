bb30a96e3f4153b41b1e6c27e2a9050bb4ff0013
navneetnair7 <navneetnair95@gmail.com>
2024-05-04 12:04:25 +0530

    Scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
 import Link from "next/link";
 
 export default function HomePage() {
+  const mockUrls = [
+    "https://utfs.io/f/0d6c8843-5336-4793-b44c-8003f20a7aa4-u67e10.png",
+  ];
+
+  const mockImages = mockUrls.map((url, index) => ({
+    id: index + 1,
+    url,
+  }));
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt="" className="w-full" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
