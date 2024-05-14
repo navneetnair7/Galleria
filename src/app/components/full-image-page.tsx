0b79f4934fb02ef7720092d7715f253cb36cf405
navneetnair7 <navneetnair95@gmail.com>
2024-05-14 12:37:53 +0530

    Finish modal

--- src/app/components/full-image-page.tsx
+++ src/app/components/full-image-page.tsx
@@ -1,14 +1,24 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
+  const uploaderInfo = await clerkClient.users.getUser(image.userId);
   return (
     <div className="flex h-full w-full min-w-0 bg-red-200">
-      <div className="flex-shrink flex justify-center items-center">
-        <img src={image.url} className="w-96 flex-shrink object-contain" />
+      <div className="flex flex-shrink items-center justify-center">
+        <img src={image.url} className="flex-shrink object-contain" />
       </div>
-      <div className="flex w-48 flex-col flex-shrink-0 border-l">
-        <div className="text-xl font-bold">{image.name}</div>
+      <div className="flex w-48 flex-shrink-0 flex-col border-l">
+        <div className="border-b p-2 text-center text-lg">{image.name}</div>
+        <div className="flex flex-col px-2">
+          <span>Uploaded By:</span>
+          <span>{uploaderInfo.fullName}</span>
+        </div>
+        <div className="flex flex-col px-2">
+          <span>Created on:</span>
+          <span>{new Date(image.createdAt).toLocaleDateString()}</span>
+        </div>
       </div>
     </div>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,12 +44,12 @@ export default function RootLayout({
            */
           routerConfig={extractRouterConfig(ourFileRouter)}
         />
-        <body className="flex flex-col gap-4">
-          <TopNav />
+        <body className="grid-rows-[auto, 1fr] grid h-screen">
           <div>
-            {children}
-            {modal}
+            <TopNav />
+            <main className="overflow-y-scroll">{children}</main>
           </div>
+          {modal}
           <div id="modal-root" />
         </body>
       </html>
